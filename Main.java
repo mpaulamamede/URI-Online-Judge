7c7724634935102b9e46b64a1d4a2f718c327cda
mpaulamamede <mpaulamamede@gmail.com>
2021-08-13 16:36:40 -0300

    pequena correção de identação

--- Main.java
+++ Main.java
@@ -21,7 +21,7 @@ public class Main {
     }    
  
     public static void main(String[] args) throws IOException {
-                String r;
+        String r;
         Scanner input = new Scanner(System.in);
         r = input.nextLine();
         area(r);        
