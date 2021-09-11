cc5a20acf35f71c9756cf4942c2822533e5bf906
mpaulamamede <mpaulamamede@gmail.com>
2021-09-11 18:22:28 -0300

    exercicio media ponderada

--- wAverage.py
+++ wAverage.py
@@ -2,7 +2,9 @@
 # lê variáveis a e b e calcula média ponderada
 a = float(input())
 b = float(input())
-wa = 3.5 # peso 1
-wb = 7.5 # peso 2
-media = (wa*a+wb*b)/10
-print("MEDIA = {0:.5f}".format(media))
+c = float(input())
+wa = 2 # pesos
+wb = 3 
+wc = 5 
+media = (wa*a+wb*b+wc*c)/(wa+wb+wc)
+print("MEDIA = {0:.1f}".format(media))
