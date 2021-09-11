582028b15ee7cbbddbdd8fbd393106acd11bfdb6
mpaulamamede <mpaulamamede@gmail.com>
2021-09-11 18:17:02 -0300

    add ex média ponderada

---
+++ wAverage.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# lê variáveis a e b e calcula média ponderada
+a = float(input())
+b = float(input())
+wa = 3.5 # peso 1
+wb = 7.5 # peso 2
+media = (wa*a+wb*b)/10
+print("MEDIA = {0:.5f}".format(media))
