b01f8cc0e6a89ac63eb0795f588ef787f840825b
mpaulamamede <mpaulamamede@gmail.com>
2021-09-11 18:29:34 -0300

    exercício produto e diferença

---
+++ difference.py
@@ -0,0 +1,8 @@
+# lê quatro variaveis
+# calcula diferença entre o produto de a, b e c, d
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+dif = (a*b-c*d)
+print("DIFERENCA = {}".format(dif))
